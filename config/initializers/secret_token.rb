044d116a6ef6a97b37e126b2d5f616bece111e94
Justin Coyne <justin@curationexperts.com>
2013-08-20 13:42:41 -0500

    Generate rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CurateDce::Application.config.secret_key_base = 'e51f2bcb45a66f06e8539ed0e0fbe8ca236cf5ecab52bb7a9d8104ab137c7e7bde448e44f4904ccf837b819ee05631e842dda62ed922abdb438482e7c1d5c3db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CurateDce::Application.config.session_store :cookie_store, key: '_curate_dce_session'
